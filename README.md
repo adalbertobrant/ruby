22e672e51dbb356cd0b0e6fea2971ff7ed2f54b8
Adalbertobrant <adalbertobrant@gmail.com>
2019-06-25 22:01:38 -0300

    programa apresentacao em ruby usando variavies e mostrando na tela

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Um Estudo  das propriedades do Ruby uma linguagem melhor que o Python.
 A linguagem Ruby como a Python necessita de muito estudo e dedicaçao.
 O python e uma boa linguagem, mas o Ruby tem se mostrado superior.
+<p>O objetivo desse diretorio e agregar o basico da linguagem ruby e futuramente de seu framework mais famoso o Rails.</p>
 
---
+++ apresentacao.rb
@@ -0,0 +1,25 @@
+# Tutorial 1 - Programa que mostra na tela nome , idade, cidade
+
+# variaveis em Ruby 
+
+nome = "Seu nome aqui" 
+idade = 100
+cidade = "Sua cidade aqui"
+
+# explicacao em ruby , todas as variaveis sao consideradas OBJETOS, ver POO google ai ...
+# o '#' eh como fazemos um comentario em ruby de uma linha, caso queira mais linhas pode fazer assim 
+=begin
+   vc vai comentando aqui e muda de linha
+   muda de linha 
+   muda de linha de novo e termina com 
+=end
+
+# o ruby entende que ' =begin ' e ' =end ' sao o inicio e fim do comentario
+
+# mostrando na tela, com o comando puts
+
+puts "Meu nome e #{nome}!"
+puts "Tenho #{idade} anos!"
+puts "Moro em #{cidade}!"
+
+# o puts sempre salta uma linha, para nao saltar linhas deve se usar o print
